a4e4e96ebc37bd23c6f8a1f2e6f6bcc2c7aea5c5
jaroid <imthanhhai217@gmail.com>
2023-08-25 20:38:42 +0700

    -add login

--- app/src/main/java/com/jaroidx/chatapp/LoginActivity.java
+++ app/src/main/java/com/jaroidx/chatapp/LoginActivity.java
@@ -3,12 +3,28 @@ package com.jaroidx.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.edtUserName)
+    EditText edtUserName;
+    @BindView(R.id.edtPassword)
+    EditText edtPassword;
+    @BindView(R.id.btnLogin)
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
+
     }
 }
